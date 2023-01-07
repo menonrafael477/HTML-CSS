6154772787d61c44119025c95e852b04b77408d4
Rafael Menon <menonrafael7@gmail.com>
2023-01-06 22:50:06 -0300

    Softgraf

---
+++ Softgraf-Calculadora/introduction.js
@@ -0,0 +1,11 @@
+function say() {
+    console.log('Hello World');
+}
+
+// say();
+
+function even(number) {
+    return number % 2 === 0;
+}
+
+var isEven = even(10);
---
+++ Softgraf-Calculadora/script.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.getElementById('value');
+    const error = document.querySelector('#error')
+    input.focus();
+    var somado = false;
+
+    document.querySelectorAll('.button').forEach(button => {
+        button.onclick = () => {
+            const operation = button.dataset.operation;
+            const number = button.dataset.number;
+            error.getElementsByClassName.display = 'none';
+
+            if(somado) {
+                input.value = '';
+                somado = false;
+        }
+
+        if(operation == 'clear') {
+            input.value = '';
+            input.focus()
+            return;
+        }
+
+        if(operation == '=') {
+            somado = true;
+            var result = ''
+            try {
+                result = eval(String(input.value))
+            }   catch (error) {
+                input.value = '';
+                error.style.display = 'block';
+                return;
+
+            }
+
+            if(isFinite(result)) {
+                input.value = result.toFixed(3);
+            } else {
+                input.value = '';
+                error.style.display = 'block';
+            }
+            return;
+          }
+
+          5
+          if(operation) {
+            input.value += String(operation);
+          } else {
+            input.value += String(number);
+          }
+        };
+    });
+});
